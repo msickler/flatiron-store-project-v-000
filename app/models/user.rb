743c04a4a3a453987a22cf4a01f54fd36808c549
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-15 17:25:26 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         #:omniauthable, :omniauth_providers => [:github]
+         :omniauthable, :omniauth_providers => [:github]
 
   #def self.from_omniauth(auth)
   #  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
