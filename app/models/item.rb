eef83241eeae4087b0d0ad41a80a9e2dc0c2770c
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-15 18:16:12 +0000

    automatically backed up by learn

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,8 +3,5 @@ class Item < ActiveRecord::Base
   has_many :line_items
   has_many :carts, through: :line_items
 
-  def self.available_items
-    self.all.select { |item| item.inventory >= 1 }
-  end
-
+  
 end
