4280ea95406cfda315c48ca3ffad690f77ef0267
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-15 17:39:02 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :logged_in?, :current_cart
 
+  def logged_in?
+    !!current_user
+  end
+ 
+  def verify_user
+    redirect_to new_user_registration_path unless logged_in?
+  end
+ 
+  def current_cart
+    current_user.current_cart if current_user
+  end
+
   #def after_sign_in_path_for(resource)
   #  request.env['omniauth.origin'] || store_path
   #end
