4aa3257373c7930f0be91636a8b398cc9fcbce04
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-15 18:06:30 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,12 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:github]
 
-  #def self.from_omniauth(auth)
-  #  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-  #    user.email = auth.info.email
-  #    user.password = Devise.friendly_token[0,20]
-  #  end
-  #end
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
 
   def create_current_cart
     new_cart = self.carts.create
