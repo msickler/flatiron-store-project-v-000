1c31da97212da9af12b884c07177b4cd594429af
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-15 18:13:57 +0000

    automatically backed up by learn

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,11 +1,10 @@
 class LineItemsController < ApplicationController
   def create
-
     if current_user.current_cart == nil
       current_user.current_cart = current_user.carts.create
       current_user.save
     end
-    current_user.current_cart.add_item(params[:item_id]).save
+    current_user.current_cart.add_item(params[:item_id])
     current_user.current_cart.save
     redirect_to cart_path(current_user.current_cart)
   end
